88cbbde523d4177d0758b33acd08f71779f4178b
Mohd Shahalam <officialshahalam855@gmail.com>
2024-07-22 21:45:35 +0530

    fg

--- src/components/core/home/CourseCard.jsx
+++ src/components/core/home/CourseCard.jsx
@@ -10,7 +10,7 @@ function CourseCard({courseData,currentCard,setCurrentCard}) {
         onClick={()=>{setCurrentCard(courseData)}}
         className={
             `flex flex-col justify-between pb-4 bg-white space-y-3 w-[310px] h-[300px] 
-            ${courseData===currentCard ?"bg-white shadow-[12px_12px_0px_0px_#FFD60A]":"bg-[#161d27]"}`
+            ${courseData===currentCard ?"bg-white shadow-[12px_12px_0px_0px_#FFD60A]":"bg-black"}`
         }>
         <div className='px-6 pt-6 pb-24 border-b border-dashed space-y-4'>
             <h1 className={`text-xl font-semibold ${courseData==currentCard?"text-richblack-900":"text-richblack-300"}`}>{courseData.heading}</h1>
@@ -23,7 +23,7 @@ function CourseCard({courseData,currentCard,setCurrentCard}) {
                 <p>beginner</p>
             </div>
             <div className={`flex gap-4 items-center font-medium ${courseData==currentCard?"text-blue-400":""}`}>
-                {/* <TbBinaryTree2/> */}
+                <TbBinaryTree2/>
                 <p>6 Lessons</p>
             </div>
         </div>
