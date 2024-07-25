5b8e2fda784f13d0f35775bcaf5599cc8c399ccd
Mohd Shahalam <officialshahalam855@gmail.com>
2024-07-25 11:35:42 +0530

    aaa

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -3,9 +3,6 @@ const dotenv=require("dotenv");
 
 dotenv.config();
 
-console.log("key id",process.env.RAZORPAY_KEY);
-console.log("key id",process.env.RAZORPAY_SECRET);
-
 
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
